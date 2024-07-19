f4fe6ccef57bbe1e5153efa5283206aab75e8e11
the-code-machine <sarthak25ic049@satiengg.in>
2024-07-19 20:35:10 +0530

    update

--- src/components/utils/WhatsApp.tsx
+++ src/components/utils/WhatsApp.tsx
@@ -10,8 +10,8 @@ const WhatsApp: React.FC<WhatsAppProps> = ({ }) => {
     return (
         <>
 
-            <Link href="https://wa.me/918818828823" className=" rounded-full  bg-[#26C943] border-tertiary-200 border flex justify-center items-center fixed w-14 h-14 bottom-5 right-5  md:bottom-10  z-[100] md:right-10" target="_blank">
-                <motion.div whileHover={{ scale: 1.15 }} whileTap={{ scale: 0.9 }}>  <FaWhatsapp color='#fff' size={35} /></motion.div>
+            <Link href="https://wa.me/918818828823" className=" rounded-full font-semibold  bg-[#26C943]  text-white flex justify-center space-x-2 items-center fixed px-4 py-3 bottom-5 right-5  md:bottom-10  z-[100] md:right-10" target="_blank">
+                <motion.div whileHover={{ scale: 1.15 }} whileTap={{ scale: 0.9 }}>  <FaWhatsapp color='#fff' size={30} /> </motion.div><span className=' capitalize'>Chat with us! 24/7</span>
             </Link>
         </>
     );
